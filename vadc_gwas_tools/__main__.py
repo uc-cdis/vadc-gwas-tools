7754d34c7de3182b2d10e5fa1621b7f1c8bb424a
Kyle Hernandez <kmhernan84@gmail.com>
2022-05-04 11:38:56 -0400

    chore(setup): setup entrypoint and logging

---
+++ vadc_gwas_tools/__main__.py
@@ -0,0 +1,46 @@
+"""
+Main entrypoint for all vadc-gwas-tools.
+"""
+import argparse
+import datetime
+import sys
+from signal import SIG_DFL, SIGPIPE, signal
+
+from vadc_gwas_tools.logger import Logger
+
+signal(SIGPIPE, SIG_DFL)
+
+
+def main(args=None):
+    """
+    The main method for vadc-gwas-tools.
+    """
+    # Setup logger
+    Logger.setup_root_logger()
+
+    logger = Logger.get_logger("main")
+
+    # Print header
+    logger.info("-" * 75)
+    logger.info("Program Args: vadc-gwas-tools " + " ".join(sys.argv[1::]))
+    logger.info("Date/time: {0}".format(datetime.datetime.now()))
+    logger.info("-" * 75)
+    logger.info("-" * 75)
+
+    # Get args
+    p = argparse.ArgumentParser("VADC GWAS Tools")
+    subparsers = p.add_subparsers(dest="subcommand")
+    subparsers.required = True
+
+    options = p.parse_args(args)
+
+    # Run
+    cls = options.func(options)
+    cls.do_work()
+
+    # Finish
+    logger.info("Finished!")
+
+
+if __name__ == "__main__":
+    main()
