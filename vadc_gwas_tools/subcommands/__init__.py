e1955a8be2666f7077c5faf10e560fe98004ff06
Kyle Hernandez <kmhernan84@gmail.com>
2022-05-09 17:50:41 -0400

    feat(cohort): auto import

--- vadc_gwas_tools/subcommands/__init__.py
+++ vadc_gwas_tools/subcommands/__init__.py
@@ -2,4 +2,5 @@ from __future__ import absolute_import
 
 from .base import Subcommand
 from .filter_segments import FilterSegments
+from .get_cohort_pheno_csv import GetCohortPheno
 from .split_filename_by_chromosome import SplitFilenameByChr
