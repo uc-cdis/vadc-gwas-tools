e358c41531fe9964aa2dc7a2fe58d2c615dd8a72
Kyle Hernandez <kmhernan84@gmail.com>
2022-05-12 14:43:03 -0400

    chore(tests): add gzip csv test to cohort csv subcommand

--- tests/test_subcommand_get_cohort_pheno_csv.py
+++ tests/test_subcommand_get_cohort_pheno_csv.py
@@ -1,5 +1,6 @@
 """Tests for the ``vadc_gwas_tools.subcommands.GetCohortPheno`` subcommand"""
 import csv
+import gzip
 import tempfile
 import unittest
 from typing import List, NamedTuple, Optional
@@ -67,6 +68,11 @@ class TestGetCohortPhenoSubcommand(unittest.TestCase):
                 ]
                 self.assertEqual(client.get_cohort_csv.call_args_list, exp_calls)
 
+            with self.assertRaises(OSError) as _:
+                with gzip.open(fpath1, "rt") as fh:
+                    for line in fh:
+                        pass
+
             with open(fpath1, "rt") as fh:
                 reader = csv.DictReader(fh)
                 self.assertEqual(
@@ -130,6 +136,68 @@ class TestGetCohortPhenoSubcommand(unittest.TestCase):
                     mock.call(1, 3, tmp_control_path, ["ID_1001", "ID_1002"]),
                 ]
                 self.assertEqual(client.get_cohort_csv.call_args_list, exp_calls)
+        finally:
+            cleanup_files([fpath1, tmp_case_path, tmp_control_path])
+
+    def test_process_case_control_gzip(self):
+        client = CohortServiceClient()
+        client.get_cohort_csv = mock.MagicMock(return_value=None)
+
+        (_, tmp_case_path) = tempfile.mkstemp()
+        (_, tmp_control_path) = tempfile.mkstemp()
+        (_, fpath1) = tempfile.mkstemp(suffix=".csv.gz")
+
+        with open(tmp_case_path, "wt") as o:
+            writer = csv.writer(o)
+            writer.writerow(["sample.id", "ID_1001", "ID_1002"])
+            writer.writerow([1, 100.0, 300.0])
+            writer.writerow([2, 300.0, 400.0])
+        with open(tmp_control_path, "wt") as o:
+            writer = csv.writer(o)
+            writer.writerow(["sample.id", "ID_1001", "ID_1002"])
+            writer.writerow([3, 200.0, 400.0])
+            writer.writerow([4, 500.0, 300.0])
+        try:
+            with mock.patch("tempfile.mkstemp") as mock_tmpfile:
+                mock_tmpfile.side_effect = [
+                    ("a", tmp_case_path),
+                    ("b", tmp_control_path),
+                ]
+                MOD._process_case_control(
+                    client, 1, 2, 3, ["ID_1001", "ID_1002"], fpath1, None
+                )
+                self.assertEqual(client.get_cohort_csv.call_count, 2)
+                exp_calls = [
+                    mock.call(1, 2, tmp_case_path, ["ID_1001", "ID_1002"]),
+                    mock.call(1, 3, tmp_control_path, ["ID_1001", "ID_1002"]),
+                ]
+                self.assertEqual(client.get_cohort_csv.call_args_list, exp_calls)
+
+            with gzip.open(fpath1, "rt") as fh:
+                reader = csv.DictReader(fh)
+                self.assertEqual(
+                    reader.fieldnames,
+                    ["sample.id", "ID_1001", "ID_1002", "CASE_CONTROL"],
+                )
+                curr = next(reader)
+                self.assertEqual(curr["sample.id"], "1")
+                self.assertEqual(curr["ID_1001"], "100.0")
+                self.assertEqual(curr["CASE_CONTROL"], "1")
+
+                curr = next(reader)
+                self.assertEqual(curr["sample.id"], "2")
+                self.assertEqual(curr["ID_1001"], "300.0")
+                self.assertEqual(curr["CASE_CONTROL"], "1")
+
+                curr = next(reader)
+                self.assertEqual(curr["sample.id"], "3")
+                self.assertEqual(curr["ID_1001"], "200.0")
+                self.assertEqual(curr["CASE_CONTROL"], "0")
+
+                curr = next(reader)
+                self.assertEqual(curr["sample.id"], "4")
+                self.assertEqual(curr["ID_1001"], "500.0")
+                self.assertEqual(curr["CASE_CONTROL"], "0")
         finally:
             cleanup_files([fpath1, tmp_case_path, tmp_control_path])
 
