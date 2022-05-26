71a93d6657c79139cb806145346b33dd3c23b518
Viktorija Zaksas <vzpgb@uchicago.edu>
2022-05-26 12:08:19 -0500

    (feat/create-indexd-record): Added indexd class to the commons module

---
+++ vadc_gwas_tools/common/indexd.py
@@ -0,0 +1,49 @@
+"""Small class for interacting with the indexd service to create the indexd
+record.  This tool works only for internal URLs.
+"""
+import os
+from typing import Tulpe
+
+import requests
+
+from vadc_gwas_tools.common.const import (
+    GEN3_ENVIRONMENT_KEY,
+    INDEXD_PASSWORD,
+    INDEXD_USER,
+)
+from vadc_gwas_tools.common.logger import Logger
+
+
+class IndexdServiceClient:
+    def __init__(self):
+        self.gen3_environment = os.environ.get(GEN3_ENVIRONMENT_KEY, "default")
+        self.service_url = f"http://indexd-service.{self.gen3_environment}"
+        self.logger = Logger.get_logger("IndexdServiceClient")
+
+    def get_header(self) -> Dict[str, str]:
+        "FIXME: not needed)"
+        hdr = {"Content-Type": "application/json"}
+        return hdr
+
+    def get_auth(self) -> Tulpe(str, str):
+        "Get indexd authentication"
+        indexd_user = os.environ.get(INDEXD_USER, "")
+        indexd_password = os.environ.get(INDEXD_PASSWORD, "")
+        auth = (indexd_user, indexd_password)
+        return auth
+
+    def create_indexd_record(self, metadata="", _di=requests):
+        """
+        Creates indexd record from the metadata provided.
+        Metadata should be be valid JSON.  More information on indexd metadata
+        can be found:
+            https://github.com/uc-cdis/indexd#indexd-records
+        Returns Globally Unique Identifier (GUID) assigned to the record.
+        """
+
+        self.logger.info(f"Metadata - {metadata}")
+        response = _di.post(f"{self.service_url}/index", json=metadata, auth=get_auth())
+        response.raise_for_status()
+        guid = response.json()["did"]
+        self.logger.info(f"Created GUID {guid}")
+        return guid
