d6cb6a1f5e2587e7b12c23a47aa833b83acb56b7
Viktorija Zaksas <vzpgb@uchicago.edu>
2022-06-08 15:45:57 -0500

    feat(create-indexd-record): Fix typos

--- tests/test_common_indexd.py
+++ tests/test_common_indexd.py
@@ -60,7 +60,7 @@ class TestIndexdServiceClient(TestCase):
             "file_name": "key",
             "hashes": {"md5": "8b9942cf415384b27cadf1f4d2d682e5"},
             "size": "123",
-            "urls": ["ss3://endpointurl/bucket/key"],
+            "urls": ["s3://endpointurl/bucket/key"],
             "urls_metadata": {"s3://endpointurl/bucket/key": {}},
             "form": "object",
         }
@@ -126,7 +126,7 @@ class TestIndexdServiceClient(TestCase):
             "Authentication doesn't match when environment INDEXD keys are absent",
         )
 
-    @patch('requests.post', side_effect=mocked_requests_post)
+    @patch("requests.post", side_effect=mocked_requests_post)
     def test_create_indexd_record_nouser(self, mock_post):
         "Test create_indexd_record when no INDEXD user/password provided"
         os.environ.pop(INDEXD_USER, None)
@@ -136,13 +136,13 @@ class TestIndexdServiceClient(TestCase):
             HTTPError, client.create_indexd_record, metadata=self.metadata
         )
 
-    @patch('requests.post', side_effect=mocked_requests_post)
+    @patch("requests.post", side_effect=mocked_requests_post)
     def test_create_indexd_record_nometadata(self, mock_post):
         "Test create_indexd_record when no metadata provided"
         client = ISC()
         self.assertRaises(HTTPError, client.create_indexd_record)
 
-    @patch('requests.post', side_effect=mocked_requests_post)
+    @patch("requests.post", side_effect=mocked_requests_post)
     def test_create_indexd_record(self, mock_post):
         "Test create_indexd_record with metadata and INDEXD user/password"
         client = ISC()
