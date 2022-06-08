75462035709af071988ef8d68c178ecc3b72542e
Viktorija Zaksas <vzpgb@uchicago.edu>
2022-06-08 10:33:03 -0500

    feat(create-indexd-record): Refactoring tests

--- tests/test_common_indexd.py
+++ tests/test_common_indexd.py
@@ -1,7 +1,8 @@
 """This modules tests `vadc_gwas_tools.common.indexd` classes."""
 import json
 import os
-import unittest
+from unittest import TestCase
+from unittest.mock import patch
 
 from requests import HTTPError
 
@@ -29,7 +30,7 @@ def mocked_requests_post(*args, **kwargs):
     if kwargs.get("auth") == ("TESTUSER", "TESTPASS"):
         try:
             json.loads(kwargs.get("json"))
-            # Everything looks good
+            # Everything looks good, return data and status 200 response
             return MockResponse(
                 {
                     "baseid": "e044a62c-fd60-4203-b1e5-a62d1005f027",
@@ -48,12 +49,22 @@ def mocked_requests_post(*args, **kwargs):
     return MockResponse(None, 404)
 
 
-class TestIndexdServiceClient(unittest.TestCase):
+class TestIndexdServiceClient(TestCase):
     def setUp(self):
         super().setUp()
         os.environ[GEN3_ENVIRONMENT_KEY] = "default"
         os.environ[INDEXD_USER] = "TESTUSER"
         os.environ[INDEXD_PASSWORD] = "TESTPASS"
+        metadata = {
+            "authz": ["/programs/test/projects/test"],
+            "file_name": "key",
+            "hashes": {"md5": "8b9942cf415384b27cadf1f4d2d682e5"},
+            "size": "123",
+            "urls": ["ss3://endpointurl/bucket/key"],
+            "urls_metadata": {"s3://endpointurl/bucket/key": {}},
+            "form": "object",
+        }
+        self.metadata = json.dumps(metadata)
 
     def tearDown(self):
         os.environ.pop(GEN3_ENVIRONMENT_KEY, None)
@@ -62,9 +73,8 @@ class TestIndexdServiceClient(unittest.TestCase):
         super().tearDown()
 
     def test_init_noenv(self):
-        if GEN3_ENVIRONMENT_KEY in os.environ:
-            del os.environ[GEN3_ENVIRONMENT_KEY]
-
+        "Test __init__ when no environment key provided"
+        os.environ.pop(GEN3_ENVIRONMENT_KEY, None)
         client = ISC()
         self.assertEqual(
             client.gen3_environment,
@@ -79,6 +89,7 @@ class TestIndexdServiceClient(unittest.TestCase):
         )
 
     def test_init_wenv(self):
+        "Test __init__ when with environment key provided"
         os.environ[GEN3_ENVIRONMENT_KEY] = "something-else"
         client = ISC()
         self.assertEqual(
@@ -93,56 +104,55 @@ class TestIndexdServiceClient(unittest.TestCase):
         )
         del os.environ[GEN3_ENVIRONMENT_KEY]
 
-    def test_get_auth_no_user(self):
-        # No INDEXD user/password
-        os.environ.pop(INDEXD_USER, None)
-        os.environ.pop(INDEXD_PASSWORD, None)
-        auth = ISC().get_auth()
-        self.assertEqual(
-            auth,
-            ("", ""),
-            "Authentication doesn't match when environment INDEXD keys are absent",
-        )
-
-        # With INDEXD user/password
-        os.environ[INDEXD_USER] = "TESTUSER"
-        os.environ[INDEXD_PASSWORD] = "TESTPASS"
-        auth = ISC().get_auth()
+    def test_get_auth_wuser(self):
+        "Test get_auth() when INDEXD user/password provided"
+        client = ISC()
+        auth = client.get_auth()
         self.assertEqual(
             auth,
             ("TESTUSER", "TESTPASS"),
             "Authentication doesn't match when environment INDEXD keys are present",
         )
 
-    @unittest.mock.patch(
+    def test_get_auth_nouser(self):
+        "Test get_auth() when INDEXD user/password not provided"
+        os.environ.pop(INDEXD_USER, None)
+        os.environ.pop(INDEXD_PASSWORD, None)
+        client = ISC()
+        auth = client.get_auth()
+        self.assertEqual(
+            auth,
+            ("", ""),
+            "Authentication doesn't match when environment INDEXD keys are absent",
+        )
+
+    @patch(
+        'vadc_gwas_tools.common.indexd.requests.post', side_effect=mocked_requests_post
+    )
+    def test_create_indexd_record_nouser(self, mock_post):
+        "Test create_indexd_record when no INDEXD user/password provided"
+        os.environ.pop(INDEXD_USER, None)
+        os.environ.pop(INDEXD_PASSWORD, None)
+        client = ISC()
+        self.assertRaises(
+            HTTPError, client.create_indexd_record, metadata=self.metadata
+        )
+
+    @patch(
+        'vadc_gwas_tools.common.indexd.requests.post', side_effect=mocked_requests_post
+    )
+    def test_create_indexd_record_nometadata(self, mock_post):
+        "Test create_indexd_record when no metadata provided"
+        client = ISC()
+        self.assertRaises(HTTPError, client.create_indexd_record)
+
+    @patch(
         'vadc_gwas_tools.common.indexd.requests.post', side_effect=mocked_requests_post
     )
     def test_create_indexd_record(self, mock_post):
-        metadata = {
-            "authz": ["/programs/test/projects/test"],
-            "file_name": "key",
-            "hashes": {"md5": "8b9942cf415384b27cadf1f4d2d682e5"},
-            "size": "123",
-            "urls": ["ss3://endpointurl/bucket/key"],
-            "urls_metadata": {"s3://endpointurl/bucket/key": {}},
-            "form": "object",
-        }
-        metadata = json.dumps(metadata)
-
+        "Test create_indexd_record with metadata and INDEXD user/password"
         client = ISC()
-
-        # INDEXD user/password are absent
-        os.environ.pop(INDEXD_USER, None)
-        os.environ.pop(INDEXD_PASSWORD, None)
-        self.assertRaises(HTTPError, client.create_indexd_record, metadata=metadata)
-
-        # INDEXD user/password present, no metadata provided
-        os.environ[INDEXD_USER] = "TESTUSER"
-        os.environ[INDEXD_PASSWORD] = "TESTPASS"
-        self.assertRaises(HTTPError, client.create_indexd_record)
-
-        # Normal flow with INDEXD user/password and metadata provided
-        res = client.create_indexd_record(metadata=metadata)
+        res = client.create_indexd_record(metadata=self.metadata)
         expected = {
             "baseid": "e044a62c-fd60-4203-b1e5-a62d1005f027",
             "did": "e044a62c-fd60-4203-b1e5-a62d1005f028",
