8317e3e947f229a92200e6a1fe7d7c1635f1047e
qiongl-CTDS <51421191+qiongl-CTDS@users.noreply.github.com>
2022-11-17 19:26:30 -0600

    Add files via upload

--- tests/test_subcommand_get_cohort_attrition_table.py
+++ tests/test_subcommand_get_cohort_attrition_table.py
@@ -30,12 +30,20 @@ class TestGetCohortAttritionTableSubcommand(unittest.TestCase):
         self.continuous_variable_list = [
             {"variable_type": "concept", "concept_id": 1001},
             {"variable_type": "concept", "concept_id": 1002},
-            {"variable_type": "custom_dichotomous", "cohort_ids": [10, 20], "provided_name": "test123"}
+            {
+                "variable_type": "custom_dichotomous",
+                "cohort_ids": [10, 20],
+                "provided_name": "test123",
+            },
         ]
         self.binary_variable_list = [
-            {"variable_type": "custom_dichotomous", "cohort_ids": [10, 20], "provided_name": "test123"},
+            {
+                "variable_type": "custom_dichotomous",
+                "cohort_ids": [10, 20],
+                "provided_name": "test123",
+            },
             {"variable_type": "concept", "concept_id": 1001},
-            {"variable_type": "concept", "concept_id": 1002}
+            {"variable_type": "concept", "concept_id": 1002},
         ]
         self.continuous_outcome = self.continuous_variable_list[0]
         self.binary_outcome = self.binary_variable_list[0]
@@ -58,7 +66,10 @@ class TestGetCohortAttritionTableSubcommand(unittest.TestCase):
                 variable_list_str,
                 object_hook=CohortServiceClient.decode_concept_variable_json,
             )
-            outcome_json = json.loads(json.dumps(args.outcome))
+            outcome_val = json.loads(
+                json.dumps(self.continuous_outcome),
+                object_hook=CohortServiceClient.decode_concept_variable_json,
+            )
 
             with mock.patch(
                 "vadc_gwas_tools.subcommands.get_attrition_csv.CohortServiceClient"
@@ -68,7 +79,7 @@ class TestGetCohortAttritionTableSubcommand(unittest.TestCase):
                 instance = mock_client.return_value
                 instance.get_attrition_breakdown_csv.return_value = None
                 mock_json.load.return_value = variable_objects[:]
-                mock_json.loads.return_value = outcome_json
+                mock_json.loads.return_value = outcome_val
                 # Call main()
                 MOD.main(args)
 
@@ -101,35 +112,50 @@ class TestGetCohortAttritionTableSubcommand(unittest.TestCase):
                 variable_list_str,
                 object_hook=CohortServiceClient.decode_concept_variable_json,
             )
-            outcome_json = json.loads(json.dumps(args.outcome))
+            outcome_val = json.loads(
+                json.dumps(self.binary_outcome),
+                object_hook=CohortServiceClient.decode_concept_variable_json,
+            )
 
             # Additional variable object needs to be inserted after case-cohort
-            # variable object to get the overlap between case/control and 
+            # variable object to get the overlap between case/control and
             # source cohort
+            '''
+            control_variable_list, case_variable_list = MOD._get_case_control_variable_lists_(
+                variable_objects,
+                outcome_val,
+                300
+            )
+            '''
             control_variable_list = variable_objects[:]
             new_control_dvar = CustomDichotomousVariableObject(
                 variable_type="custom_dichotomous",
-                cohort_ids=[20, 300],  # case cohort id, source id
-                provided_name="Control cohort only"
+                cohort_ids=[20, 300],
+                provided_name="Control cohort only",
             )
             control_variable_list.insert(1, new_control_dvar)
             case_variable_list = variable_objects[:]
             new_case_dvar = CustomDichotomousVariableObject(
                 variable_type="custom_dichotomous",
-                cohort_ids=[10, 300],  # control cohort id, source id
-                provided_name="Case cohort only"
+                cohort_ids=[10, 300],
+                provided_name="Case cohort only",
             )
             case_variable_list.insert(1, new_case_dvar)
+            #mock_binary_list.return_value = (control_variable_list, case_variable_list)
 
             with mock.patch(
                 "vadc_gwas_tools.subcommands.get_attrition_csv.CohortServiceClient"
             ) as mock_client, mock.patch(
                 "vadc_gwas_tools.subcommands.get_attrition_csv.json"
-            ) as mock_json:
+            ) as mock_json, mock.patch(
+                "vadc_gwas_tools.subcommands.get_attrition_csv.GetCohortAttritionTable._get_case_control_variable_lists_"
+            ) as mock_binary_list:
                 instance = mock_client.return_value
                 instance.get_attrition_breakdown_csv.return_value = None
                 mock_json.load.return_value = variable_objects
-                mock_json.loads.return_value = outcome_json
+                mock_json.loads.return_value = outcome_val
+                mock_binary_list.return_value = (control_variable_list, case_variable_list)
+                
                 # call main()
                 MOD.main(args)
 
@@ -154,3 +180,20 @@ class TestGetCohortAttritionTableSubcommand(unittest.TestCase):
                 )
         finally:
             cleanup_files(fpath1)
+
+    def test_get_control_case_variable_list(self):
+        variable_list_str = json.dumps(self.binary_variable_list)
+        variable_objects = json.loads(
+            variable_list_str,
+            object_hook=CohortServiceClient.decode_concept_variable_json,
+        )
+        outcome_val = json.loads(
+            json.dumps(self.binary_outcome),
+            object_hook=CohortServiceClient.decode_concept_variable_json,
+        )
+        (
+            control_variable_list,
+            case_variable_list,
+        ) = MOD._get_case_control_variable_lists_(variable_objects, outcome_val, 300)
+        self.assertEqual(control_variable_list[1].cohort_ids, [20, 300])
+        self.assertEqual(case_variable_list[1].cohort_ids, [10, 300])
