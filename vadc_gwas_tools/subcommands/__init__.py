7b372294224ad8151c5ae34853fb2907d1fb540c
Aarti Venkat <aartiv@uchicago.edu>
2024-12-17 13:51:32 -0600

    subcommands init

--- vadc_gwas_tools/subcommands/__init__.py
+++ vadc_gwas_tools/subcommands/__init__.py
@@ -6,6 +6,7 @@ from .curate_gwas_hits import CurateGwasHits
 from .filter_segments import FilterSegments
 from .get_attrition_csv import GetCohortAttritionTable
 from .get_cohort_pheno_csv import GetCohortPheno
+from .get_descriptive_statistics import GetDescriptiveStatistics
 from .get_gwas_metadata import GetGwasMetadata
 from .get_pheweb_plot_json import GetPheWebPlotJson
 from .process_input_variables import ProcessInputVariables
