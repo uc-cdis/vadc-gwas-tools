3760d1ca228975d5980fb59c2dce9252d8c62a28
wangfan860 <fan1@uchicago.edu>
2025-03-06 11:40:33 -0600

    change response

--- tests/test_subcommand_get_descriptive_statistics.py
+++ tests/test_subcommand_get_descriptive_statistics.py
@@ -113,137 +113,204 @@ from vadc_gwas_tools.subcommands import GetDescriptiveStatistics as MOD
 
 #         finally:
 #             cleanup_files([fpath1, fpath2])
-class MockArgs:
-    def __init__(
-        self,
-        source_id,
-        source_population_cohort,
-        variables_json,
-        outcome,
-        prefixed_breakdown_concept_id,
-        output_csv_prefix,
-        output_combined_json,
-        hare_population=None,
-    ):
-        self.source_id = source_id
-        self.source_population_cohort = source_population_cohort
-        self.variables_json = variables_json
-        self.outcome = outcome
-        self.prefixed_breakdown_concept_id = prefixed_breakdown_concept_id
-        self.output_csv_prefix = output_csv_prefix
-        self.output_combined_json = output_combined_json
-        self.hare_population = hare_population
+# class MockArgs:
+#     def __init__(
+#         self,
+#         source_id,
+#         source_population_cohort,
+#         variables_json,
+#         outcome,
+#         prefixed_breakdown_concept_id,
+#         output_csv_prefix,
+#         output_combined_json,
+#         hare_population=None,
+#     ):
+#         self.source_id = source_id
+#         self.source_population_cohort = source_population_cohort
+#         self.variables_json = variables_json
+#         self.outcome = outcome
+#         self.prefixed_breakdown_concept_id = prefixed_breakdown_concept_id
+#         self.output_csv_prefix = output_csv_prefix
+#         self.output_combined_json = output_combined_json
+#         self.hare_population = hare_population
 
 
-class TestGetDescriptiveStatistics(unittest.TestCase):
-    def setUp(self):
-        self.client = CohortServiceClient()
-        self.continuous_variable_list = [
-            {"variable_type": "concept", "concept_id": 1001},
-            {"variable_type": "concept", "concept_id": 1002},
-            {
-                "variable_type": "custom_dichotomous",
-                "cohort_ids": [10, 20],
-                "provided_name": "test123",
-            },
+# class TestGetDescriptiveStatistics(unittest.TestCase):
+#     def setUp(self):
+#         self.client = CohortServiceClient()
+#         self.continuous_variable_list = [
+#             {"variable_type": "concept", "concept_id": 1001},
+#             {"variable_type": "concept", "concept_id": 1002},
+#             {
+#                 "variable_type": "custom_dichotomous",
+#                 "cohort_ids": [10, 20],
+#                 "provided_name": "test123",
+#             },
+#         ]
+#         self.hare_population = "non-Hispanic Asian"
+#         self.hare_concept_id = 2000007029
+#         self.prefixed_breakdown_concept_id = "ID_3"
+#         self.mock_response = [
+#             [
+#                 [
+#                     {'statistics': [{'key': 'value'}]},
+#                     {'statistics': [{'key': 'value'}]},
+#                     {},
+#                 ],
+#                 [
+#                     {'statistics': [{'key': 'value'}]},
+#                     {'statistics': [{'key': 'value'}]},
+#                     {},
+#                 ],
+#                 [
+#                     {'statistics': [{'key': 'value'}]},
+#                     {'statistics': [{'key': 'value'}]},
+#                     {},
+#                 ],
+#             ]
+#         ]
+
+#     @mock.patch("vadc_gwas_tools.common.cohort_middleware.requests.post")
+#     @mock.patch(
+#         "vadc_gwas_tools.common.cohort_middleware.CohortServiceClient.get_concept_id_by_population"
+#     )
+#     @mock.patch(
+#         "vadc_gwas_tools.common.cohort_middleware.CohortServiceClient.get_header"
+#     )
+#     @mock.patch(
+#         "vadc_gwas_tools.common.cohort_middleware.CohortServiceClient.strip_concept_prefix"
+#     )
+#     def test_get_descriptive_statistics(
+#         self,
+#         mock_strip_concept_prefix,
+#         mock_get_header,
+#         mock_get_concept_id_by_population,
+#         mock_post,
+#     ):
+#         # Prepare test arguments
+#         (_, fpath1) = tempfile.mkstemp()
+#         (_, fpath2) = tempfile.mkstemp()
+#         try:
+#             with open(fpath1, 'wt') as o:
+#                 json.dump(self.continuous_variable_list, o)
+
+#             args = MockArgs(
+#                 source_id=2,
+#                 source_population_cohort=300,
+#                 variables_json=fpath1,
+#                 outcome=self.continuous_variable_list[0],
+#                 prefixed_breakdown_concept_id=self.prefixed_breakdown_concept_id,
+#                 output_csv_prefix=fpath2,
+#                 output_combined_json="/some/path/combined.json",
+#                 hare_population=self.hare_population,
+#             )
+
+#             # Map variable list to correct objects
+#             variable_objects = []
+#             for var in self.continuous_variable_list:
+#                 if var["variable_type"] == "concept":
+#                     variable_objects.append(ConceptVariableObject(**var))
+#                 elif var["variable_type"] == "custom_dichotomous":
+#                     variable_objects.append(CustomDichotomousVariableObject(**var))
+
+#             # Mock behavior
+#             mock_get_concept_id_by_population.return_value = self.hare_concept_id
+#             mock_strip_concept_prefix.return_value = [1003]
+#             mock_get_header.return_value = {"Authorization": "Bearer test-token"}
+#             mock_post.return_value.status_code = 200
+#             mock_post.return_value.json.return_value = self.mock_response
+
+#             # Call the method
+#             response = self.client.get_descriptive_statistics(
+#                 source_id=args.source_id,
+#                 cohort_definition_id=args.source_population_cohort,
+#                 local_path=args.output_csv_prefix,
+#                 variable_objects=variable_objects,
+#                 prefixed_breakdown_concept_id=args.prefixed_breakdown_concept_id,
+#                 hare_population=args.hare_population,
+#             )
+
+#             # Assertions
+#             mock_get_concept_id_by_population.assert_called_once_with(
+#                 args.source_id, args.hare_population, mock.ANY
+#             )
+#             mock_strip_concept_prefix.assert_called_once_with(
+#                 args.prefixed_breakdown_concept_id
+#             )
+#             mock_post.assert_called_with(
+#                 f"{self.client.service_url}/cohort-stats/by-source-id/{args.source_id}/by-cohort-definition-id/{args.source_population_cohort}/by-concept-id/1002",
+#                 data=mock.ANY,
+#                 headers={"Authorization": "Bearer test-token"},
+#                 stream=True,
+#                 timeout=(6.05, len(variable_objects) * 180),
+#             )
+#             self.assertEqual(response, self.mock_response)
+#         finally:
+#             cleanup_files([fpath1, fpath2])
+@mock.patch(
+    "vadc_gwas_tools.subcommands.get_descriptive_stats.CohortServiceClient.get_descriptive_statistics"
+)
+@mock.patch("vadc_gwas_tools.subcommands.get_descriptive_stats.json.load")
+@mock.patch("vadc_gwas_tools.subcommands.get_descriptive_stats.json.loads")
+def test_get_descriptive_statistics(
+    self, mock_json_loads, mock_json_load, mock_get_descriptive_statistics
+):
+    (_, fpath1) = tempfile.mkstemp()
+    (_, fpath2) = tempfile.mkstemp()
+
+    try:
+        # Prepare the JSON file with variable definitions
+        with open(fpath1, 'wt') as o:
+            json.dump(self.continuous_variable_list, o)
+
+        args = MockArgs(
+            source_id=2,
+            source_population_cohort=300,
+            variables_json=fpath1,
+            outcome=json.dumps(self.continuous_variable_list[0]),
+            prefixed_breakdown_concept_id=self.prefixed_breakdown_concept_id,
+            output_csv_prefix=fpath2,
+            output_combined_json="/some/path/combined.json",
+            hare_population=self.hare_population,
+        )
+
+        # Setup mocked responses
+        mock_json_load.return_value = [
+            ConceptVariableObject(**var)
+            if var["variable_type"] == "concept"
+            else CustomDichotomousVariableObject(**var)
+            for var in self.continuous_variable_list
         ]
-        self.hare_population = "non-Hispanic Asian"
-        self.hare_concept_id = 2000007029
-        self.prefixed_breakdown_concept_id = "ID_3"
-        self.mock_response = [
-            [
-                [
-                    {'statistics': [{'key': 'value'}]},
-                    {'statistics': [{'key': 'value'}]},
-                    {},
-                ],
-                [
-                    {'statistics': [{'key': 'value'}]},
-                    {'statistics': [{'key': 'value'}]},
-                    {},
-                ],
-                [
-                    {'statistics': [{'key': 'value'}]},
-                    {'statistics': [{'key': 'value'}]},
-                    {},
-                ],
-            ]
-        ]
 
-    @mock.patch("vadc_gwas_tools.common.cohort_middleware.requests.post")
-    @mock.patch(
-        "vadc_gwas_tools.common.cohort_middleware.CohortServiceClient.get_concept_id_by_population"
-    )
-    @mock.patch(
-        "vadc_gwas_tools.common.cohort_middleware.CohortServiceClient.get_header"
-    )
-    @mock.patch(
-        "vadc_gwas_tools.common.cohort_middleware.CohortServiceClient.strip_concept_prefix"
-    )
-    def test_get_descriptive_statistics(
-        self,
-        mock_strip_concept_prefix,
-        mock_get_header,
-        mock_get_concept_id_by_population,
-        mock_post,
-    ):
-        # Prepare test arguments
-        (_, fpath1) = tempfile.mkstemp()
-        (_, fpath2) = tempfile.mkstemp()
-        try:
-            with open(fpath1, 'wt') as o:
-                json.dump(self.continuous_variable_list, o)
+        mock_json_loads.return_value = ConceptVariableObject(
+            **self.continuous_variable_list[0]
+        )
 
-            args = MockArgs(
-                source_id=2,
-                source_population_cohort=300,
-                variables_json=fpath1,
-                outcome=self.continuous_variable_list[0],
-                prefixed_breakdown_concept_id=self.prefixed_breakdown_concept_id,
-                output_csv_prefix=fpath2,
-                output_combined_json="/some/path/combined.json",
-                hare_population=self.hare_population,
-            )
+        # Correctly structured mock response (no extra nesting)
+        mock_get_descriptive_statistics.return_value = {
+            "statistics": [{"key": "value"}]
+        }
 
-            # Map variable list to correct objects
-            variable_objects = []
-            for var in self.continuous_variable_list:
-                if var["variable_type"] == "concept":
-                    variable_objects.append(ConceptVariableObject(**var))
-                elif var["variable_type"] == "custom_dichotomous":
-                    variable_objects.append(CustomDichotomousVariableObject(**var))
+        # Execute the method being tested (MOD.main)
+        MOD.main(args)
 
-            # Mock behavior
-            mock_get_concept_id_by_population.return_value = self.hare_concept_id
-            mock_strip_concept_prefix.return_value = [1003]
-            mock_get_header.return_value = {"Authorization": "Bearer test-token"}
-            mock_post.return_value.status_code = 200
-            mock_post.return_value.json.return_value = self.mock_response
+        # Verify the client call was made correctly
+        mock_get_descriptive_statistics.assert_called_once_with(
+            args.source_id,
+            args.source_population_cohort,
+            f"{args.output_csv_prefix}.descriptive_stats.json",
+            mock_json_load.return_value,
+            args.prefixed_breakdown_concept_id,
+            args.hare_population,
+        )
 
-            # Call the method
-            response = self.client.get_descriptive_statistics(
-                source_id=args.source_id,
-                cohort_definition_id=args.source_population_cohort,
-                local_path=args.output_csv_prefix,
-                variable_objects=variable_objects,
-                prefixed_breakdown_concept_id=args.prefixed_breakdown_concept_id,
-                hare_population=args.hare_population,
-            )
+        # Verify the output file is correctly written
+        output_json_path = f"{args.output_csv_prefix}.descriptive_stats.json"
+        with open(output_json_path, 'rt') as f:
+            result = json.load(f)
 
-            # Assertions
-            mock_get_concept_id_by_population.assert_called_once_with(
-                args.source_id, args.hare_population, mock.ANY
-            )
-            mock_strip_concept_prefix.assert_called_once_with(
-                args.prefixed_breakdown_concept_id
-            )
-            mock_post.assert_called_with(
-                f"{self.client.service_url}/cohort-stats/by-source-id/{args.source_id}/by-cohort-definition-id/{args.source_population_cohort}/by-concept-id/1002",
-                data=mock.ANY,
-                headers={"Authorization": "Bearer test-token"},
-                stream=True,
-                timeout=(6.05, len(variable_objects) * 180),
-            )
-            self.assertEqual(response, self.mock_response)
-        finally:
-            cleanup_files([fpath1, fpath2])
+        # Assert that the output JSON matches your mock
+        self.assertEqual(result, {"statistics": [{"key": "value"}]})
+
+    finally:
+        cleanup_files([fpath1, fpath2, output_json_path])
